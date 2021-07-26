fc9e87578e4e8634afda341ca9812de0602da836
TangYM <Yuanzhi.Tang@cn.unisys.com>
2021-07-26 22:57:50 +0800

    Add OCP Bad Samples

---
+++ Design-Principles/Open-Closed-Principle/BadSample/GraphicEditor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_Principles.Open_Closed_Principle.BadSample
+{
+	public class GraphicEditor
+	{
+		public void DrawAllShapes(IList<Shape> shapes)
+		{
+			foreach(var shape in shapes)
+			{
+				if(shape.Type == ShapeType.Circle)
+				{
+					DrawCircle(shape);
+				}
+				else if(shape.Type == ShapeType.Square)
+				{
+					DrawSquare(shape);
+				}
+				//else if(shape.Type == ShapeType.Triangle)
+				//{
+				//	DrawTriangle(shape);
+				//}
+			}
+		}
+
+		private void DrawSquare(Shape shape)
+		{
+			//throw new NotImplementedException();
+		}
+
+		private void DrawCircle(Shape shape)
+		{
+			//throw new NotImplementedException();
+		}
+	}
+}
---
+++ Design-Principles/Open-Closed-Principle/OCPDemo.cs
@@ -0,0 +1,17 @@
+﻿using Design_Principles.Open_Closed_Principle.BadSample;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_Principles.Open_Closed_Principle
+{
+	public class OCPDemo
+	{
+		public static void Run()
+		{
+			GraphicEditorBadSample.Run();
+		}
+	}
+}

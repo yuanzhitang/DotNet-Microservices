25bd6f1a8af33661e516e97e76dfe8178100fd70
chengsha <Sha.Cheng@cn.unisys.com>
2021-08-13 09:35:04 +0800

    Interface-segregation-principle

---
+++ Design-Principles/Interface-Segregation-Principle/GoodExample/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace Design_Principles.Interface_Segregation_Principle.GoodSample
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var driver = new Driver(new Car());
+            driver.Drive();
+        }
+    }
+}

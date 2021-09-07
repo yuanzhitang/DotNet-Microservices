e1cb47ba05989a0d053aaa67d3bcdad1d4e8d964
wushuai <Shuai.Wu@unisys.com>
2021-09-07 11:50:22 +0800

    Chain Of Responsibility

---
+++ Design-Patterns/Behavioral/ChainOfResponsibility/CORDemo.cs
@@ -0,0 +1,24 @@
+﻿using Design_Patterns.Behavioral.ChainOfResponsibility.Sample;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_Patterns.Behavioral.ChainOfResponsibility
+{
+	public class CORDemo : PatternDemoBase
+	{
+		public override string PatternName => "Chain of Responsibilty ";
+
+		protected override void RunImpl()
+		{
+			VacationCOR requestChainOfResponsibility = new VacationCOR();
+			int[] requests = {1,2, 5, 14, 22, 18, 3, 27, 20 };
+			foreach (int request in requests)
+			{
+				requestChainOfResponsibility.handleRequest(request);
+			}
+		}
+	}
+}
